428a456a3971631498dba98e89b496d95434ad79
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-01-11 22:12:21 +0530

    Added File Output and Improved User Interaction with Inquirer

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
-// using node modules such as inquirer and qr-image create a qr code generator 
-
 import inquirer from 'inquirer';
 import qr from 'qr-image';
-import fs from 'fs'
+import fs from 'fs';
 
+// Function to generate and save QR code
+const generateQRCode = (url, size) => {
+    const qrImage = qr.image(url, { size });
+    qrImage.pipe(fs.createWriteStream('qr_img.png'));
+};
+
+const saveURLToFile = (url) => {
+    fs.writeFile('url-text.txt', url, (err) => {
+        if (err) throw err;
+        console.log('URL saved to file!');
+    });
+};
 
 inquirer
     .prompt([
-        // question is an object which takes some key value pairs, 
-        // using message property which will be shown as a prompt.
-        // and URL for url
         {
-            "message": "Please type in your URL:",
-            "name": "URL"
-        }])
+            message: 'Please type in your URL:',
+            name: 'URL',
+        },
+    ])
     .then((answers) => {
-        // after the prompt 
-        // saved the url in variable
         const url = answers.URL;
-
-        // converted url into qr image of size 4
-        let qr_png = qr.image(url, { size: 4 });
-
-        // writing image using fs module into the directory
-        qr_png.pipe(fs.createWriteStream('qr_img.png'));
-
-        // writing the url in
-        fs.writeFile('url-text.txt', url, (err)=> {
-            if(err) throw err;
-            console.log('File saved!')
-        })
+        generateQRCode(url, 4);
+        saveURLToFile(url);
     })
     .catch((error) => {
-        console.log(error);
+        console.error(error);
     });
